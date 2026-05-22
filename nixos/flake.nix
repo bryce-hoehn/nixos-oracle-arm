98b54cf03e44bc4877788d1678951adc54c442d6
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-22 15:04:09 -0400

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,6 +6,7 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
+    # TARGET_FLAKE_CONFIG
     nixosConfigurations.oci-nixos-arm = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
