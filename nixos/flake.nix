cf2898b68fc4cfd4da22a6afb147a3aed75d9a5e
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-22 14:29:30 -0400

    Create flake.nix

---
+++ nixos/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS configuration flake for Oracle Cloud ARM Free Tier";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.oci-nixos-arm = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
