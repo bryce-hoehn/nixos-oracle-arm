46cdc7a8b462c5eea723859a3e12a64854fec358
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-22 15:00:04 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS QCOW2 Image Builder for Oracle Cloud ARM64 Free Tier";
+  description = "NixOS QCOW2 Image Builder running natively on ARM64 runners";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -10,31 +10,18 @@
   };
 
   outputs = { self, nixpkgs, nixos-generators, ... }: {
-    packages.x86_64-linux.oci-arm64 = nixos-generators.lib.nixosGenerate {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    packages.aarch64-linux.oci-arm64 = nixos-generators.lib.nixosGenerate {
+      pkgs = nixpkgs.legacyPackages.aarch64-linux;
+      
       modules = [
-        "${nixpkgs}/nixos/modules/virtualisation/oci-image.nix"
+        "${nixpkgs}/nixos/modules/virtualisation/oracle-compute-config.nix"
+        ./nixos/configuration.nix
 
         ({ pkgs, ... }: {
           nixpkgs.hostPlatform = "aarch64-linux";
           networking.useDHCP = nixpkgs.lib.mkForce true;
           networking.predictableInterfaceNames = true;
-          boot.loader.systemd-boot.enable = true;
-          boot.loader.efi.canTouchEfiVariables = true;
 
-          # Bootstrap SSH config so you can log in the first time
-          services.openssh.enable = true;
-          users.users.opc = {
-            isNormalUser = true;
-            description = "Oracle Cloud User";
-            extraGroups = [ "networkmanager" "wheel" ];
-            openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI...your_key_here..."
-            ];
-          };
-          security.sudo.wheelNeedsPassword = false;
-
-          # Read files from your repo and write them straight into /etc/nixos/
           environment.etc = {
             "nixos/flake.nix".text = builtins.readFile ./nixos/flake.nix;
             "nixos/configuration.nix".text = builtins.readFile ./nixos/configuration.nix;
