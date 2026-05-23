2850f3e2ce7344cbb5fead719fccbb7ee916990c
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-23 02:32:18 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,54 +1,64 @@
 {
-  description = "NixOS QCOW2 Image Builder running natively on ARM64 runners";
+  description = "NixOS QEMU-UEFI disk image for Oracle Cloud Arm shapes";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    packages.aarch64-linux.oracle = 
-      let
-        systemConfig = nixpkgs.lib.nixosSystem {
-          modules = [
-            "${nixpkgs}/nixos/modules/virtualisation/oci-image.nix"
-            ./nixos/configuration.nix
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "aarch64-linux";
 
-            ({ pkgs, lib, ... }: {
-              
-              services.cloud-init.enable = true;
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
 
-              nixpkgs.overlays = [
-                # strips the "kvm" feature restriction for the runner
-                (final: prev: {
-                  nixos-disk-image = prev.nixos-disk-image.overrideAttrs (oldAttrs: {
-                    requiredSystemFeatures = [ ];
-                  });
-                })
+          "${nixpkgs}/nixos/modules/image/images.nix"
 
-                # https://github.com/nix-community/nixos-generators/issues/443#issuecomment-3697547318
-                (final: prev: {
-                  lkl = prev.lkl.overrideAttrs (old: {
-                    postPatch = (old.postPatch or "") + ''
-                      substituteInPlace tools/lkl/cptofs.c \
-                        --replace-fail 'lkl_start_kernel("mem=100M")' 'lkl_start_kernel("mem=1024M")'
-                    '';
-                  });
-                })
-              ];
+          ({ ... }: {
+            image.modules = {
+              qemu-efi = { };
+            };
+          })
 
-              nixpkgs.hostPlatform = "aarch64-linux";
-              networking.useDHCP = lib.mkForce true;
-              networking.usePredictableInterfaceNames = true;
+          ({ pkgs, lib, ... }: {
+            services.cloud-init.enable = true;
 
-              environment.etc = {
-                "nixos/flake.nix".text = builtins.readFile ./flake.nix;
-                "nixos/configuration.nix".text = builtins.readFile ./nixos/configuration.nix;
-                "nixos/hardware-configuration.nix".text = builtins.readFile ./nixos/hardware-configuration.nix;
-              };
-            })
-          ];
-        };
-      in
-      systemConfig.config.system.build.image; 
-  };
+            nixpkgs.overlays = [
+              # strips the "kvm" feature restriction for the runner
+              (final: prev: {
+                nixos-disk-image = prev.nixos-disk-image.overrideAttrs (_oldAttrs: {
+                  requiredSystemFeatures = [ ];
+                });
+              })
+
+              # https://github.com/nix-community/nixos-generators/issues/443#issuecomment-3697547318
+              (final: prev: {
+                lkl = prev.lkl.overrideAttrs (old: {
+                  postPatch = (old.postPatch or "") + ''
+                    substituteInPlace tools/lkl/cptofs.c \
+                      --replace-fail 'lkl_start_kernel("mem=100M")' 'lkl_start_kernel("mem=1024M")'
+                  '';
+                });
+              })
+            ];
+
+            nixpkgs.hostPlatform = "aarch64-linux";
+            networking.useDHCP = lib.mkForce true;
+            networking.usePredictableInterfaceNames = true;
+
+            environment.etc = {
+              "nixos/flake.nix".text = builtins.readFile ./flake.nix;
+              "nixos/configuration.nix".text = builtins.readFile ./nixos/configuration.nix;
+              "nixos/hardware-configuration.nix".text = builtins.readFile ./nixos/hardware-configuration.nix;
+            };
+          })
+        ];
+      };
+    in
+    {
+      packages.${system}.oracle =
+        nixos.config.system.build.images.qemu-efi;
+    };
 }
