3e09f67a08bf397430fa0549892d5e507da0f8c0
bryce <bryce.hoehn@icloud.com>
2026-05-24 00:54:39 -0400

    sfsf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,7 @@ terraform {
   backend "s3" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
     force_path_style            = true
   }
 }
